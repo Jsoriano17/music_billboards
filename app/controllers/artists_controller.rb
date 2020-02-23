35098a39545d9f3a4af12e2bd35ac6568a7f3195
Jeffrey Soriano <jeffsor17@gmail.com>
2020-02-22 21:42:47 -0700

    working CRUD for artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,13 +1,48 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :edit, :update, :destroy]
   def index
+    @artists = Artist.all
   end
 
   def show
   end
 
   def new
+    @artist = Artist.new
+    render partial: 'form'
+  end
+
+  def create
+    @artist = Artist.new(set_params)
+    if @artist.save
+      redirect_to artists_path
+    else
+      render :new
+    end
   end
 
   def edit
+    render partial: 'form'
+  end
+
+  def update
+    if @artist.update(set_params)
+      redirect_to artists_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy 
+    @artist.destroy
+    redirect_to artists_path
+  end
+
+  private 
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+  def set_params
+    params.require(:artist).permit(:name)
   end
 end
