9403433306264e3c84c722578108f3ef138d2b88
Jeffrey Soriano <jeffsor17@gmail.com>
2020-02-22 21:52:09 -0700

    added song model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'songs/index'
+  get 'songs/show'
+  get 'songs/new'
+  get 'songs/edit'
   root 'artists#index'
   resources :artists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_032311) do
+ActiveRecord::Schema.define(version: 2020_02_23_044815) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -18,4 +18,21 @@ ActiveRecord::Schema.define(version: 2020_02_23_032311) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "names", force: :cascade do |t|
+    t.integer "artist_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["artist_id"], name: "index_names_on_artist_id"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.integer "artist_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
+  end
+
+  add_foreign_key "names", "artists"
+  add_foreign_key "songs", "artists"
 end
